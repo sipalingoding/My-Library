0644c9fee9aba27cc2e002c94054b4ce8b08de7f
sipalingoding <arsenalsalmanfpl@gmail.com>
2025-01-14 21:45:35 +0700

    until submit form profile to supabse

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,20 +1,43 @@
 "use server";
 
 import { profileSchemas } from "./schemas";
+import db from "./db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+
+    if (!user) throw new Error("please login with user");
+
     const rawData = Object.fromEntries(formData);
-    console.log(rawData);
     const profileSchema = profileSchemas.parse(rawData);
 
-    console.log("data sukse", profileSchema);
-    return { message: "profile created!" };
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user?.imageUrl ?? "",
+        ...profileSchema,
+      },
+    });
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
     console.log(error);
-    return { message: "there was an error" };
+    return {
+      message: error instanceof Error ? error.message : "there will be error",
+    };
   }
+
+  redirect("/");
 };
