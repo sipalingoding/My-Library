59b5668d66ec2b23f71798f2e8a6cc8ba8cf2cf6
sipalingoding <arsenalsalmanfpl@gmail.com>
2025-01-18 16:55:14 +0700

    add function fetchProfile

--- utils/actions.ts
+++ utils/actions.ts
@@ -56,3 +56,38 @@ export const fetchProfileImage = async () => {
 
   return profileImage?.profileImage;
 };
+
+export const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("you must be logged in");
+  }
+  if (user.privateMetadata.hasProfile) {
+    redirect("/profile/create");
+  }
+
+  return user;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!profile) {
+    redirect("/profile/create");
+  }
+
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevData: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "update profile action" };
+};
